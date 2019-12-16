e1f6cd92ec98f4858ecbf5bdb7b940d08a940d4d
noaElish <48222620+noaElish@users.noreply.github.com>
2019-12-16 19:44:04 +0200

    Update Command.cpp

--- Command.cpp
+++ Command.cpp
@@ -1,9 +1,13 @@
 //
-// Created by noa on 12/12/2019.
+// Created by noa on 16/12/2019.
 //
+
+#ifndef EX3__COMMAND_H_
+#define EX3__COMMAND_H_
+#include <iostream>
+
 #include <thread>
 #include <sys/socket.h>
-#include <iostream>
 #include <netinet/in.h>
 #include <vector>
 #include <unistd.h>
@@ -21,66 +25,51 @@ class Command {
   ~Command();
 };
 
+//function of open Command
+void openDataServer(int port){
+  //creating new socket
+  int socketServer= socket(AF_INET, SOCK_DGRAM, 0 );
+  if (socketServer == -1){
+    //cant create socket
+    std::cerr <<"cant create socket"<<std::endl;
+  }
+  //create the object for the bind
+  sockaddr_in addressSer;
+  addressSer.sin_family= AF_INET;
+  addressSer.sin_addr.s_addr= INADDR_ANY;
+  addressSer.sin_port= htons(port);
+
+  //bind the socket to ip
+  if (bind(socketServer, (struct sockaddr *) &addressSer, sizeof(addressSer))==-1 ){
+    std::cerr <<"cant bind socket to ip"<<std::endl;
+  }
+
+  //listen to the port
+  if (listen(socketServer,5)==-1){
+    std::cerr <<"cant listen to ip"<<std::endl;
+  }
+
+  //accept the client- flight gear
+  if (accept(socketServer, (struct sockaddr*)&addressSer, (socklen_t*)&addressSer)==-1){
+    std::cerr <<"cant accept client"<<std::endl;
+  }
+
+  //reading from the socket line by line-use loop
+  //////////////////////fill//////////////////////
+
+  //closing the socket
+  close(socketServer);
+}
+
 
 class OpenServerCommand : public Command{
-
  public:
   virtual int execute(vector<string> arrayStr){
-
-
     //open thread to to connect
     ////////change- need to get the port to this function instead of 5400///////
-    std::thread threadServer(openDataServer(5400));
-
+    std::thread threadServer(openDataServer,5400);
   }
-
-
-  //open thread, read the lines and store the data.
-  int openDataServer(int port){
-    //creating new socket
-    int socketServer= socket(AF_INET, SOCK_DGRAM, 0 );
-    if (socketServer == -1){
-      //cant create socket
-      std::cerr <<"cant create socket"<<std::endl;
-    }
-    //create the object for the bind
-    sockaddr_in addressSer;
-    addressSer.sin_family= AF_INET;
-    addressSer.sin_addr.s_addr= INADDR_ANY;
-    addressSer.sin_port= htons(port);
-
-    //bind the socket to ip
-    if (bind(socketServer, (struct sockaddr *) &addressSer, sizeof(addressSer))==-1 ){
-      std::cerr <<"cant bind socket to ip"<<std::endl;
-        return -2;
-    }
-
-    //listen to the port
-    if (listen(socketServer,5)==-1){
-      std::cerr <<"cant listen to ip"<<std::endl;
-      return -3;
-    }
-
-    //accept the client- flight gear
-    if (accept(socketServer, (struct sockaddr*)&addressSer, (socklen_t*)&addressSer)==-1){
-      std::cerr <<"cant accept client"<<std::endl;
-      return -4;
-    }
-
-    //reading from the socket line by line-use loop
-    //////////////////////fill//////////////////////
-
-
-
-
-    //closing the socket
-    close(socketServer);
-  }
-
-  //destructor
   //~OpenServerCommand;
-
-
 };
 
 class ConnectCommand : public Command{
@@ -103,4 +92,4 @@ class DefineVarCommand : public Command{
   //~DefineVarCommand;
 };
 
-
+#endif //EX3__COMMAND_H_
