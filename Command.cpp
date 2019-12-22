9751673d9ab3bb47dbf9a7016046440708377057
shiraTurgeman <58763845+shiraTurgeman@users.noreply.github.com>
2019-12-22 23:13:11 +0200

    Add files via upload

--- Command.cpp
+++ Command.cpp
@@ -1,4 +1,5 @@
 
+
 #include "Command.h"
 #include "Interpreter.h"
 #include "Expression.h"
@@ -32,84 +33,70 @@ void openDataServer(int port) {
     addressSer.sin_port = htons(port);
 
     //bind the socket to ip
-    if (bind(socketServer, (struct sockaddr *) &addressSer, sizeof(addressSer)) == -1) {
+    int binding = bind(socketServer, (struct sockaddr *) &addressSer, sizeof(addressSer));
+    if (binding == -1) {
         std::cerr << "cant bind socket to ip" << std::endl;
     }
 
     //listen to the port
-    if (listen(socketServer, 5) == -1) {
+    int listenning = listen(socketServer, 5);
+    if (listenning == -1) {
         std::cerr << "cant listen to ip" << std::endl;
     }
 
+
+
+
+    int acc = accept(socketServer, (struct sockaddr *)&addressSer, (socklen_t *)&addressSer);
+
     //accept the client- flight gear
-    int clientSock = accept(socketServer, (struct sockaddr *) &addressSer, (socklen_t *) &addressSer);
-    if (clientSock == -1) {
+    //int clientSock = accept(socketServer, (struct sockaddr *) &addressSer, (socklen_t *) &addressSer);
+    if (acc == -1) {
         std::cerr << "cant accept client" << std::endl;
     }
-
 
 
     //reading from the socket line by line-use loop-CHECK/////////////////
 
     //save each line to the data structure
     char bufferRead[1024] = {0};
-    int value = read(clientSock, bufferRead, 1024);
-    cout<<"value:  "<< value<<endl;
-    int i;
-    string s = "";
-    /*
-    for (i = 0; i < sizeof(bufferRead) / sizeof(char); i++) {
-
-        s = s + bufferRead[i];
-    }
-     */
-    /*
-    cout<<bufferRead<<endl;
-    for (i = 0; i < sizeof(bufferRead) / sizeof(char); i++) {
-        cout<<bufferRead[i]<<i<<endl;
-        if (bufferRead[i]==','){
-            cout<<i;
-            s;
-        }
-        s = s + bufferRead[i];
-    }
-    string token;
-    int num;
-    string array[36];
-    string sim;
-    SymbolTable *simMaps = simMaps->getInstance();
-    for (i=0; i<s.size();i++){
-        char aaaa = s[i];
-        if (s[i]=='\\'){
-            token = s.substr(0, s.find('\\'));
-            num = stoi(token);
-            sim = array[num];
-            Variable *var = new Variable(num, sim);
-            simMaps->simMap.insert({sim, *var});
-
-            s = s.erase(0, s.find('\\')+1);
-            i=0;
-        }
-    }
-*/
-
-   /******************************************************************************/
+    int value;
     SymbolTable* instance = SymbolTable::getInstance();
-    float valueArray[36];
-    float num=0;
-    for (i=0; i<36; i++){
-        valueArray[i] = num;
-        num++;
-        instance->putInSimMap(i, valueArray[i]);
-        num=i;
-        //num++;
-    }
+    size_t i=0;
+    string token;
+    string s;
+    float num;
+    int index=0;
+    while(true){
+        value = read(acc, bufferRead, 1024);
+        if (value == -1) {
+            std::cerr << "error with read" << std::endl;
+        }
+        while (i< sizeof(bufferRead)){
 
-    //put sim and value in simMAp
-    for (i=0; i<36; i++){
-        instance->putInSimMap(i, valueArray[i]);
-    }
+            s = s + bufferRead[i];
+            if ((bufferRead[i]==',')){
+                token = s.substr(0, s.find(','));
+                num = stof(token);
+                instance->putInSimMap(index, num);
+                s = s.erase(0, s.find(',')+1);
+                index++;
+                //cout<<"token:  "<<token<<"   num:  "<<num<<"  index:  "<<index<<endl;
+            }
+            if ((bufferRead[i]=='\0')){
+                token = s.substr(0, s.find('\0'));
+                num = stof(token);
+                instance->putInSimMap(index, num);
+                s = s.erase(0, s.find('\0')+1);
+                index=0;
+                //cout<<"token:  "<<token<<"   num:  "<<num<<"  index:  "<<index<<endl;
+                break;
+            }
+            i++;
+        }
+        i=0;
 
+    }
     //closing the socket
     close(socketServer);
 }
@@ -179,71 +166,71 @@ ConnectCommand::~ConnectCommand() {};
 //constructor
 DefineVarCommand::DefineVarCommand(){};
 int DefineVarCommand::execute(vector<string> arrayStr, int i) {
-  /*
-  float value = 3;
-  string sim = "p";
-  string direction = "o";
-  SymbolTable *symbolsMaps = symbolsMaps->getInstance();
-  Variable variable = new Variable(value,sim, direction);
- // v= new Variable(3, "h", "o");
-  string d= "hh";
-  //symbolsMaps->symbolTable.insert(d, var);
-  symbolsMaps->putInSymbolTable(d,variable);
-  */
+    /*
+    float value = 3;
+    string sim = "p";
+    string direction = "o";
+    SymbolTable *symbolsMaps = symbolsMaps->getInstance();
+    Variable variable = new Variable(value,sim, direction);
+   // v= new Variable(3, "h", "o");
+    string d= "hh";
+    //symbolsMaps->symbolTable.insert(d, var);
+    symbolsMaps->putInSymbolTable(d,variable);
+    */
 
-  cout << "in var def" << endl;
-  /**need to change this func**///////////////////////////////////////////////////////////////////////
-  index = i;
+    cout << "in var def" << endl;
+    /**need to change this func**///////////////////////////////////////////////////////////////////////
+    index = i;
 
-  SymbolTable *instance = instance->getInstance();
+    SymbolTable *instance = instance->getInstance();
 
-  string saveName = arrayStr[i];
-  //send the string after the "=" to expression
-  Inter *i1 = new Inter();
-  Expression *next;
-  next = i1->interpret(arrayStr[i + 2]);
-  double value = next->calculate();
+    string saveName = arrayStr[i];
+    //send the string after the "=" to expression
+    Inter *i1 = new Inter();
+    Expression *next;
+    next = i1->interpret(arrayStr[i + 2]);
+    double value = next->calculate();
 
-  //check the direction
-  Variable check = instance->symbolTable.at(arrayStr[i]);
-  string dir = check.getDir();
-
-  //update the value in the first map
-  if (dir == "->") {
-    //change the value in the symbol table
-    //erase the old one
+    //check the direction
     Variable check = instance->symbolTable.at(arrayStr[i]);
-    string sim = check.getSim();
-    instance->symbolTable.erase(arrayStr[i]);
-    //create new value
-    Variable* insert= new Variable(stoi(arrayStr[i+2]), sim, dir);
-    string name= arrayStr[i];
-    instance->symbolTable.insert({name,*insert});
-    //change the value in the sim table
-    instance->simMap.erase(sim);
-    instance->simMap.insert({sim,value});
+    string dir = check.getDir();
 
-  } else if (dir == "<-") {
-      //save the value from the sim table
-      float saveValue= instance->getValueFromSim(arrayStr[i+2]);
-      //save it to the symbol table
-    Variable* insert= new Variable(saveValue, arrayStr[i+2], dir);
-    instance->symbolTable.erase(arrayStr[i]);
-    string name= arrayStr[i];
-    instance->symbolTable.insert({name,*insert});
-  }
+    //update the value in the first map
+    if (dir == "->") {
+        //change the value in the symbol table
+        //erase the old one
+        Variable check = instance->symbolTable.at(arrayStr[i]);
+        string sim = check.getSim();
+        instance->symbolTable.erase(arrayStr[i]);
+        //create new value
+        Variable* insert= new Variable(stoi(arrayStr[i+2]), sim, dir);
+        string name= arrayStr[i];
+        instance->symbolTable.insert({name,*insert});
+        //change the value in the sim table
+        instance->simMap.erase(sim);
+        instance->simMap.insert({sim,value});
+
+    } else if (dir == "<-") {
+        //save the value from the sim table
+        float saveValue= instance->getValueFromSim(arrayStr[i+2]);
+        //save it to the symbol table
+        Variable* insert= new Variable(saveValue, arrayStr[i+2], dir);
+        instance->symbolTable.erase(arrayStr[i]);
+        string name= arrayStr[i];
+        instance->symbolTable.insert({name,*insert});
+    }
 
 
-  /*if (arrayStr[index + 1].compare("=") == 0) {
-    return 2;
-  } else if ((arrayStr[index + 1].compare("while")) || (arrayStr[index + 1].compare("for")) ||
-      (arrayStr[index + 1].compare("if"))) {
-    return 1;
-  } else if ((arrayStr[index + 2].compare("sim") == 0)) {
-    return 5;
-  } else {
-    return 0;
-  }*/
+    /*if (arrayStr[index + 1].compare("=") == 0) {
+      return 2;
+    } else if ((arrayStr[index + 1].compare("while")) || (arrayStr[index + 1].compare("for")) ||
+        (arrayStr[index + 1].compare("if"))) {
+      return 1;
+    } else if ((arrayStr[index + 2].compare("sim") == 0)) {
+      return 5;
+    } else {
+      return 0;
+    }*/
 }
 //destructor
 DefineVarCommand::~DefineVarCommand() {};
@@ -316,4 +303,3 @@ int Sleep::execute(vector<string> arrayStr, int i) {
 }
 //destructor
 Sleep::~Sleep() {};
-
