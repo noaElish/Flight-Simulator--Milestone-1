7cbe85ae73fd5489b8f0669aeddc3a84af6f6fd8
noaElish <48222620+noaElish@users.noreply.github.com>
2020-01-03 01:25:41 +0200

    Update Interpreter.h

--- Interpreter.h
+++ Interpreter.h
@@ -1,4 +1,3 @@
-
 //
 // Created by shira on 19.12.2019.
 //
@@ -22,28 +21,34 @@
 #include <unistd.h>
 #include <unordered_set>
 #include <arpa/inet.h>
+#include <queue>
 using namespace std;
 
+extern queue <pair<string, float >> simQueue;
+
+extern int data_received;
+extern bool done;
+//queue <string> queueForSimulator;
 
 /**
  * Class Interpreter
  */
 class Interpreter {
-public:
-    //constructor
-    Interpreter();
-    //This function deletes unnecessary spaces from words
-    string trim(const string& str);
-    //This function divides the text into separate words and put them in array
-    vector<string> splitLine(string line, vector<string> splittedStrings );
-    //This function reads a text from a file and inserts it into an array
-    vector<string> lexer(char* fileName);
-    //This function builds a map of strings and commands
-    map <string, Command*> intoCommandMap(vector<string> splittedStrings,map <string, Command*> commandMap );
-    //This function goes on array and executes the commands
-    void parser (vector<string> splittedStrings);
-    //constructor
-    ~Interpreter();
+ public:
+  //constructor
+  Interpreter();
+  //This function deletes unnecessary spaces from words
+  string trim(const string& str);
+  //This function divides the text into separate words and put them in array
+  vector<string> splitLine(string line, vector<string> splittedStrings );
+  //This function reads a text from a file and inserts it into an array
+  vector<string> lexer(char* fileName);
+  //This function builds a map of strings and commands
+  map <string, Command*> intoCommandMap(vector<string> splittedStrings,map <string, Command*> commandMap );
+  //This function goes on array and executes the commands
+  void parser (vector<string> splittedStrings);
+  //constructor
+  ~Interpreter();
 };
 
 
@@ -51,21 +56,24 @@ public:
  * Class Variable
  */
 class Variable {
-private:
-    float value;
-    string sim;
-    string direction;
-public:
-    //constructor
-    Variable(float value, string sim, string direction);
-    void updateValue(float num);
-    string getDir();
-    string getSim();
-    float getVar();
+ private:
+  int update;
+  float value;
+  string sim;
+  string direction;
 
+ public:
+  //constructor
+  Variable(float value, string sim, string direction, int update);
+  void updateValue(float num);
+  void updateStat(int num);
+  string getDir();
+  string getSim();
+  float getVar();
+  int getUpdate();
 
-    //constructor
-    ~Variable();
+  //constructor
+  ~Variable();
 };
 
 
@@ -73,27 +81,29 @@ public:
  * Class SymbolTable
  */
 class SymbolTable{
-private:
-    static SymbolTable* instance;
-    //constructor - It is in the private area so objects cannot be created from outside.
-    SymbolTable();
-public:
-    //map to mane of variable and the Variable object
-    map<string, Variable> symbolTable;
-    map<string, float > simMap;
-    //map to commands
-    map <string, Command*> commandMap;
+ private:
+  static SymbolTable* instance;
+  //constructor - It is in the private area so objects cannot be created from outside.
+  SymbolTable();
+ public:
 
-    //This method allows you to get the pointer to the only object created.
-    static SymbolTable *getInstance();
-    void intilizationCommandMap(vector<string> splittedStrings);
-    void upDateSymbolTable(string,Variable);
-    Variable getVariable(string name);
-    string SetArrayOfSim(int i);
-    void putInSimMap (int i, float num);
-    float getValueFromSim(string sim);
-    //destructor
-    ~SymbolTable();
+  //map to mane of variable and the Variable object
+  map<string, Variable> symbolTable;
+  map<string, float > simMap;
+  //map to commands
+  map <string, Command*> commandMap;
+
+  //This method allows you to get the pointer to the only object created.
+  static SymbolTable *getInstance();
+  map<string, Command *>  intilizationCommandMap(vector<string> splittedStrings);
+  void upDateSymbolTable(string,Variable);
+  void updateValImSymbolTable(string key, float val);
+  Variable getVariable(string name);
+  string SetArrayOfSim(int i);
+  void putInSimMap (int i, float num);
+  float getValueFromSim(string sim);
+  //destructor
+  ~SymbolTable();
 };
 
 
